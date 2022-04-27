a4b67b4e7b08e0c39238ff05c9be482f2bbdcc8c
John <johnmulvey48@yahoo.com>
2022-04-27 12:45:13 -0400

    git@github.com:Jmulvez/react-hooks-fetch-crud-lab.git

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then((questions) => setQuestions(questions))
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul></ul>
     </section>
   );
 }
